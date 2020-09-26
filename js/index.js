7d4f00346efa2ac2c784b42526156d2d883e09cb
Anirudh Sridhar <as443@buffalo.edu>
2020-09-26 16:17:22 -0400

    adding flask, webpack, and react files

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.render(
+	<BrowserRouter>
+		<div>
+			Hello
+		</div>
+	</BrowserRouter>,
+	document.getElementById('app')
+)
