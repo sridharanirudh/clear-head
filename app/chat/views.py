ffc4715d7b6f7bea26d66509160b78b570d64eec
raunaqjain <jraunaq18@gmail.com>
2020-09-27 09:44:01 -0400

    Final api

--- app/chat/views.py
+++ app/chat/views.py
@@ -97,11 +97,14 @@ def fork():
 def is_good():
     if request.method == 'POST':
         req = request.json
-        yes = bool(req['answer'] == 'true')
+        user_id = req['user_id']
+        name = user[user_id]['name']
+        if req['answer'] in ['yes', 'Yes', 'Sure', 'sure', 'okay', 'Okay']:
+            yes = True
         if not yes:
-            messages = ["It was nice checking up on you. Do come by for your next session :)"]
-        else:
-            messages = "Temporary"
+            messages = ["It was nice talking to you {name}.", "Do come by for your next session :)"]
+        # else:
+            # messages = "Temporary"
             # Need to query the sessions table for all values of user_id. Generate a plot on the start level per day"
         response = {
             'messages': messages,
@@ -114,11 +117,13 @@ def is_bad():
     if request.method == 'POST':
         req = request.json
         quantity = int(req['answer'])
+        user_id = req['user_id']
+        name = user[user_id]['name']
         if quantity >= 5:
-            messages = ["Lets get you calm down.", "Take deep breaths for 2 minutes and only concentrate on the breathing."]
+            messages = ["Lets get you calm down, {name}.", "Take deep breaths for 2 minutes and only concentrate on the breathing."]
             timer = 2
         elif quantity >= 3:
-            messages = ["Lets relax first.", "Take deep breaths for 1 minute and only concentrate on the breathing."]
+            messages = ["Lets relax first, {name}.", "Take deep breaths for 1 minute and only concentrate on the breathing."]
             timer = 1
 
         ##TODO
@@ -130,6 +135,31 @@ def is_bad():
         response = {
             'messages': messages,
             'timer': timer,
-            'post_message': ['What has been bothering you lately?'],
+            'next': 'post',
         }
     return jsonify(response)
+
+@chat.route('/api/post', methods= ['POST'])
+def post():
+    if request.method == 'POST':
+        user_id = req['user_id']
+        name = user[user_id]['name']
+        messages = [f'So {name}, What has been bothering you lately?'],
+        response = {
+            'messages': messages,
+            'next': 'end'
+        }
+        return jsonify(response)
+
+@chat.route('/api/end', methods= ['POST'])
+def relax():
+    if request.method == 'POST':
+        user_id = req['user_id']
+        name = user[user_id]['name']
+        messages = [f"{name}, I hope you feel better after sharing your troubles.", "Come back anytime when you want to talk"]
+
+        response = {
+            'messages': messages,
+            'end': True
+        }
+        return jsonify(response)
